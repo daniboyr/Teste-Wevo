4497a1133d9b1c41ad0c341774dece51e21d87cb
Daniel Ruas <danielsruas@hotmail.com>
2022-12-04 16:19:32 -0300

    ARQUIVO DENTRO DO XAMP, TUDO CERTO

--- tela-de-cadastro.php
+++ tela-de-cadastro.php
@@ -1,3 +1,11 @@
+<?php
+    if(isset($_POST['submit']))
+    { 
+        print_r($_POST['nome']);
+        print_r($_POST['email']);
+        print_r($_POST['telefone']);
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -88,7 +96,7 @@
 </head>
 <body>
     <div class="box">
-        <form action="">
+        <form action="tela-de-cadastro.php" method="POST">
             <fieldset>
 
                 <legend> <b> Cadastro </b> </legend>
