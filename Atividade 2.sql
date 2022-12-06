77571e568ff884d9a7acc0b18e34641d544c0111
Daniel Ruas <109623697+daniboyr@users.noreply.github.com>
2022-12-05 23:55:42 -0300

    Update Atividade 2.sql

--- Atividade 2.sql
+++ Atividade 2.sql
@@ -6,11 +6,11 @@ CREATE DATABASE cadastro;
 CREATE TABLE clientes(
 	ID INT,
 	Nome VARCHAR(100),
-    Email VARCHAR(100),
-    CPF INT(11),
-    Telefone INT(11),
-    Sexo VARCHAR (50),
-    DataNascimento DATE
+   	Email VARCHAR(100),
+    	CPF INT(11),
+   	Telefone INT(11),
+    	Sexo VARCHAR (50),
+    	DataNascimento DATE
 );
 
 -- Adicionando dados na tabela 'clientes'
@@ -19,9 +19,9 @@ SELECT * FROM clientes;
 INSERT INTO clientes(ID, Nome, Email, CPF, Telefone, Sexo, DataNascimento)
 VALUES
 	(1, 'Ana'	, 	'ana123@gmail.com', '123456789', '55987987889', 'Feminino', '05/02/2001' 			),
-    (2, 'Bruno'	, 	'bruno_vargas@outlook.com', '123456789', '55987987889', 'Masculino', '10/05/1978'	),
-    (3, 'Carla'	, 	'carlinha1999@gmail.com', '123123123', '987123456', 'Feminino', '11/10/1995'	),
-    (4, 'Wevo'	, 	'wevosuportes@gmail.com', '6589742351', '10598874135', 'Feminino', '2012/01/01/'	);
+    	(2, 'Bruno'	, 	'bruno_vargas@outlook.com', '123456789', '55987987889', 'Masculino', '10/05/1978'	),
+   	(3, 'Carla'	, 	'carlinha1999@gmail.com', '123123123', '987123456', 'Feminino', '11/10/1995'	),
+    	(4, 'Wevo'	, 	'wevosuportes@gmail.com', '6589742351', '10598874135', 'Feminino', '2012/01/01/'	);
     
 -- Atualizando dados da tabela (UPDATE)
 SELECT * FROM clientes;
